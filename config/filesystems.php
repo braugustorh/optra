e3ea04e8f74d19e7f113728bb40e880916efad82
GSilver <geodmsgit@gsilver.com>
2026-05-30 11:27:00 -0600

    Configurando sedyco_disk, se agrega el root=optra

--- config/filesystems.php
+++ config/filesystems.php
@@ -53,6 +53,7 @@ return [
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
+            'root' => 'optra/'
         ],
 
         's3' => [
